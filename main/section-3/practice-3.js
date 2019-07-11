697ab06594d2fed55100a39587a7ccf91d9e124b
“Dean” <467271883@qq.com>
2019-07-11 13:31:17 +0800

    Add section3-practice-1 impletement

--- main/section-3/practice-3.js
+++ main/section-3/practice-3.js
@@ -1,5 +1,19 @@
 'use strict';
 
 module.exports = function createUpdatedCollection(collectionA, objectB) {
-  return '实现练习要求，并改写该行代码。';
+  let temp = collectionA.reduce((re, cur) => {
+    re[cur] ? re[cur]++ : re[cur] = 1
+    return re;
+  },{})
+  let keys = Object.keys(temp);
+  let c = [];
+  keys.forEach(key => {
+    c.push({key: key, count: temp[key]})
+  })
+  c.forEach(item => {
+    if(objectB.value.indexOf(item.key) !== -1){
+      item.count -= parseInt(item.count / 3);
+    }
+  })
+  return c;
 }
--- main/section-3/practice-4.js
+++ main/section-3/practice-4.js
@@ -1,5 +1,21 @@
 'use strict';
 
 module.exports = function createUpdatedCollection(collectionA, objectB) {
-  return '实现练习要求，并改写该行代码。';
+  let temp = collectionA.reduce((re, cur) => {
+    let letter = cur.split("-")[0];
+    let count = cur.split("-")[1] === undefined ? 1 : cur.split("-")[1]
+    re[letter] ? re[letter] += parseInt(count) : re[letter] = parseInt(count);
+    return re;
+  },{})
+  let keys = Object.keys(temp);
+  let c = [];
+  keys.forEach(key => {
+    c.push({key: key, count: temp[key]})
+  })
+  c.forEach(item => {
+    if(objectB.value.indexOf(item.key) !== -1){
+      item.count -= parseInt(item.count / 3)
+    }
+  })
+  return c;
 }
